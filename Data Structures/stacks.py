bfb803fa530cdae507b035d7ec372bf89cb0cef2
Sankar Biswas <sankarb475@gmail.com>
2019-08-05 21:42:19 +0530

    Update and rename stats.txt to stacks.py

---
+++ Data Structures/stacks.py
@@ -0,0 +1,13 @@
+A stack is a collection of objects that are inserted and removed according to the last-in, first-out (LIFO) principle.
+a stack is an abstract data type (ADT) such that an instance S supports the following operations::
+
+S.push(e): Add element e to the top of stack S.
+
+S.pop(): Remove and return the top element from the stack S; an error occurs if the stack is empty.
+
+S.top(): Return a reference to the top element of stack S, without removing it; an error occurs if the stack is empty.
+
+S.is empty( ): Return True if stack S does not contain any elements.
+
+len(S): Return the number of elements in stack S
+
