856d8fe44c3371759f7e94bcb529869deab0d88e
Sankar Biswas <sankarb475@gmail.com>
2021-07-05 19:09:06 +0530

    Create deque.py

---
+++ Data Structures/Deque/deque.py
@@ -0,0 +1,21 @@
+Deque (Doubly ended queue)
+===================================================================================
+Deque (Doubly Ended Queue) in Python is implemented using the module “collections“. Deque is preferred over list in the cases where we need quicker 
+append and pop operations from both the ends of container, as deque provides an O(1) time complexity for append and pop operations as compared to 
+list which provides O(n) time complexity.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
