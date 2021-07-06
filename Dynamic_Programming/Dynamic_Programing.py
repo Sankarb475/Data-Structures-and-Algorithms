28ea92eefffdfed0327921c3b225605156616710
Sankar Biswas <sankarb475@gmail.com>
2021-07-06 08:30:33 +0530

    Create Dynamic_Programing.py

---
+++ Dynamic_Programming/Dynamic_Programing.py
@@ -0,0 +1,14 @@
+
+Dynamic Programming - 
+-- Divding a problam into smaller sub problems.
+-- it refers to simplifying a complicated problem by breaking it down into simpler sub-problems in a recursive manner.
+
+ in computer science, if a problem can be solved optimally by breaking it into sub-problems and then recursively finding the optimal 
+ solutions to the sub-problems, then it is said to have optimal substructure.
+
+If sub-problems can be nested recursively inside larger problems, so that dynamic programming methods are applicable, then there is 
+a relation between the value of the larger problem and the values of the sub-problems.[1] In the optimization literature this relationship 
+is called the Bellman equation.
+
+
+-- Recursion is needed to resolve this problem.
